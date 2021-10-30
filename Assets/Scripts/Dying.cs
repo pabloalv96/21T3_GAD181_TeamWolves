85cb99f199edb3e4cf42c304dc8d61eef1f27bc4
Mat <1019868@student.sae.edu.au>
2021-10-31 09:00:10 +1100

    Squashed commit of the following:
    commit 18fecf78e3a3692c9330a2669c10f2030734261f
    Author: pabloalv96 <1022923@student.sae.edu.au>
    Date:   Wed Oct 27 20:28:23 2021 +1100
    
        Oxygen Timer V.2
    
        Now timer resets properly when player press "E"
    
    commit 4f8588bba30a36b04f427861e6b8cbcbbf4d937a
    Author: pabloalv96 <1022923@student.sae.edu.au>
    Date:   Mon Oct 25 17:46:07 2021 +1100
    
        Oxygen and Lights
    
    commit e3dc18571af6da2b91e0b92f5e25afdb1547e108
    Author: pabloalv96 <1022923@student.sae.edu.au>
    Date:   Wed Oct 20 20:33:10 2021 +1100
    
        Add Oxygen Timer and Slider

---
+++ Assets/Scripts/Dying.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dying : MonoBehaviour
+{ //Variables
+    [SerializeField] Transform player;
+    [SerializeField] Transform respawnPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
