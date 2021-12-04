1dcf07ad6f372b7191f54712aa62a3284cb9d870
Mat <1019868@student.sae.edu.au>
2021-12-05 10:19:40 +1100

    Updated the Alien base tile maps with and amended format

---
+++ Assets/2D Dungeon Tile Pack/Scripts/LightEffect.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightEffect : MonoBehaviour
+{
+    Light lightSource;
+    float lightIntencity;
+    [SerializeField]
+    private float minIntencity = 3f, maxIntencity = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lightSource = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lightIntencity = Random.Range(minIntencity, maxIntencity);
+        lightSource.intensity = lightIntencity;
+    }
+}
