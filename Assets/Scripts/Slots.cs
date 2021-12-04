d383506fab42b7ec9b92ea144ba5b073a7419d7d
pabloalv96 <1022923@student.sae.edu.au>
2021-12-04 15:59:21 +1100

    Win Condition V1

--- Assets/Scripts/Slots.cs
+++ Assets/Scripts/Slots.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Slots : MonoBehaviour
 {
     private Inventory inventory;
     public int i;
     public Timer timer;
+    public Win win;
+    public int winCounter=0;
 
     private void Start()
     {
@@ -22,6 +25,11 @@ public class Slots : MonoBehaviour
             DropItem();                
         }
 
+        if (Input.GetKeyDown(KeyCode.E) && win.textEnabled == true)
+        {
+            DestroyItem();
+        }
+
         if (transform.childCount <= 0)
         {
             inventory.isFull[i] = false;
@@ -35,7 +43,30 @@ public class Slots : MonoBehaviour
             child.GetComponent<Spawn>().SpawnDroppedItem();
             GameObject.Destroy(child.gameObject); 
         }
+    }
 
+    public void DestroyItem()
+    {
+        foreach (Transform child in transform)
+        {
+            GameObject.Destroy(child.gameObject);
+
+            if(inventory.isFull[0] == true || inventory.isFull[1] == true)
+            {
+                winCounter += 1;
+            }
+            
+
+            if(inventory.isFull[0] == true && inventory.isFull[1] == true)
+            {
+                winCounter += 2;
+            }
+
+            if (winCounter >= 2)
+            {
+                SceneManager.LoadScene("Main Menu");
+            }
+        }
     }
 
     
